07505b27319dc6f1631d5991f0f60e86dd87d95f
Vers <vers11@ukr.net>
2023-02-09 16:43:13 +0200

    tasks

---
+++ backend/models/User/User.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace models\User;
+
+use models\Model;
+
+class User extends Model
+{
+    public string $table = 'users';
+
+}
--- backend/routes/routes.php
+++ backend/routes/routes.php
@@ -2,11 +2,8 @@
 
 use routes\Router;
 
-Router::add('admin/posts/{id}/edit', ['posts\PostController' => 'index']);
-Router::add('posts/{id}/edit', ['posts\PostController' => 'edit']);
-Router::add('admin/posts/{id}', ['posts\PostController' => 'index']);
-Router::add('posts/{id}', ['posts\PostController' => 'show']);
 Router::add('posts', ['posts\PostController' => 'index']);
+Router::add('register', ['user\RegisterController' => 'store']);
 
 
 
