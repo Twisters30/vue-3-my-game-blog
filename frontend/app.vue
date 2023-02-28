81a3b50a74bc109579e31ac552d5097aaf6d46fb
Twiters300 <Tvist821@mail.ru>
2023-02-28 20:06:00 +0300

    axios-jwt

--- frontend/app.vue
+++ frontend/app.vue
@@ -8,9 +8,17 @@
 <script setup>
 import { useLayoutStore } from "@/stores/layout.js";
 import { useUserRoleStore } from "./stores/userRole.js";
+import axios from "axios";
 
 const layoutStore = useLayoutStore();
 const userRoleStore = useUserRoleStore();
+axios.interceptors.request.use((res) => {
+  console.log(res)
+  return res;
+},  (error) => {
+  console.log(error);
+  return Promise.reject(error);
+});
 </script>
 
 <style></style>
