fae3afb5611391e6ab436376d547704e7486ce5c
Twiters300 <Tvist821@mail.ru>
2023-03-01 06:46:39 +0300

    intercept

--- frontend/app.vue
+++ frontend/app.vue
@@ -8,17 +8,9 @@
 <script setup>
 import { useLayoutStore } from "@/stores/layout.js";
 import { useUserRoleStore } from "./stores/userRole.js";
-import axios from "axios";
 
 const layoutStore = useLayoutStore();
 const userRoleStore = useUserRoleStore();
-axios.interceptors.request.use((res) => {
-  console.log(res)
-  return res;
-},  (error) => {
-  console.log(error);
-  return Promise.reject(error);
-});
 </script>
 
 <style></style>
